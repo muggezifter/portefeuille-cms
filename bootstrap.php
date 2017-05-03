b04b6c5ac6ab07cf3210a57cf9b0646b35f435c1
muggezifter <muggezifter@gmail.com>
2017-05-03 21:14:19 +0200

    installed eloquent

---
+++ bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+require "vendor/autoload.php";
+
+use Illuminate\Database\Capsule\Manager;
+
+// get db credentials from ini file
+$dbconfig = parse_ini_file('config/db.ini');
+
+// boot eloquent
+$manager = new Manager();
+$manager->addConnection($dbconfig);
+$manager->bootEloquent();
