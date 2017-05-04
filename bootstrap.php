efc63a1684850570ef47f27dcab37092af906db3
muggezifter <muggezifter@gmail.com>
2017-05-04 14:35:20 +0200

    cleanup

--- bootstrap.php
+++ bootstrap.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Capsule\Manager;
 // get db credentials from ini file
 $dbconfig = parse_ini_file('config/db.ini');
 
+
 // boot eloquent
 $manager = new Manager();
 $manager->addConnection($dbconfig);
--- httpdocs/test.php
+++ httpdocs/test.php
@@ -2,9 +2,13 @@
 require "../bootstrap.php";
 
 use Rietveld\Models\Post;
-
-
-foreach( Post::all() as $post) {
-	echo $post->title,"<br />";
-}
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$p = new Post();
+var_dump($p->all());
+echo "x";
+//foreach( Post::all() as $post) {
+//	echo $post->title,"<br />";
+//}
 
